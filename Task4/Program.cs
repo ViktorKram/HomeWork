e842710f1f8b1d532e9a9e0cec93de3a9df5c3b2
Viktor <Viktor@DESKTOP-MPG9A87>
2022-02-26 22:19:01 +0300

    Update

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -13,82 +13,119 @@ namespace Task4
            
         }
 
-        static string DistributeTheAmount(string option, double moneyAmount, string sumList)
+        static string DistributeTheAmount(string option, double sum, string proportions)
         {
-            StringBuilder result = new StringBuilder();
-            double[] sumsFromList = sumList.Split(';').Select(x => double.Parse(x)).ToArray();
-            
-            switch (option)
+            if (double.IsNaN(sum))
+                throw new ArgumentException("Your sum is NaN.");
+            else if (double.IsPositiveInfinity(sum))
+                throw new ArgumentException("Your sum is positive infinity.");
+            else if (sum < 0)
+                throw new ArgumentException("Your sum is less than zero.");
+            else
             {
-                case "ПРОП":
-                    // Определим общую сумму значений из списка сумм.
-                    double sumListTotal = 0;
+                StringBuilder result = new StringBuilder();
+                List<string> finalSums = new List<string>();
+                double[] propArray = proportions.Split(';').Select(x => double.Parse(x)).ToArray();
 
-                    foreach (double s in sumsFromList)
-                    {
-                        sumListTotal += s;
-                    }
+                switch (option)
+                {
+                    // Разработаем сценарий для пропорционального распределения суммы.
+                    case "ПРОП":
+                        // Определим общую сумму значений из списка пропорций.
+                        double proportionSum = 0;
 
-                    // Найдем количество денег, соответствующей 1 единице пропорции.
-                    double singleCount = moneyAmount / sumListTotal;
-
-                    // Для каждой, кроме последней, суммы из списка найдем пропорциональное выражение и добавим его в результирующую строку.
-                    for (int i = 0; i < sumsFromList.Length - 1; i++)
-                    {
-                        result.Append((singleCount * sumsFromList[i]).ToString("#.##") + ";");
-
-                    }
-
-                    // Округлим остаток от суммы. 
-                    double remains = sumsFromList[sumsFromList.Length]
-                    // Добавим округленное последнее значение в конец результирующей строки. 
-                    result.Append()
-
-                    // Вернем результат в виде строки с удаленным последним символом ";".
-                    return result.Remove(result.Length - 1, 1).ToString();
-
-                case "ПЕРВ":
-                    //Начнем заполнять результирующую строку максимальными значениями с начала списка.
-                    for (int i = 0; i < sumsFromList.Length; i++)
-                    {
-                        // Если кол-во оставшихся денег меньше или равно значению в списке, добавляем остатки и устанавливаем количество денег в ноль.
-                        if (moneyAmount <= sumsFromList[i])
+                        foreach (double prop in propArray)
                         {
-                            result.Append(moneyAmount.ToString("#.##") + ";");
-                            moneyAmount = 0;
+                            proportionSum += prop;
                         }
-                        // Иначе, добавляем значение из списка в результат и вычитаем это значение из общего кол-ва денег.
-                        else
-                        {
-                            result.Append(sumsFromList[i].ToString("#.##") + ";");
-                            moneyAmount -= sumsFromList[i];
-                        }
-                    }
-                    // Вернем результат в виде строки с удаленным последним символом ";".
-                    return result.Remove(result.Length - 1, 1).ToString();
 
-                case "ПОСЛ":
-                    //Начнем заполнять результирующую строку максимальными значениями с конца списка.
-                    for (int i = sumsFromList.Length - 1; i >= 0; i--)
-                    {
-                        // Если кол-во оставшихся денег меньше или равно значению в списке, добавляем остатки и устанавливаем количество денег в ноль.
-                        if (moneyAmount <= sumsFromList[i])
+                        // Определим количество денег, соответствующей 1 единице пропорций.
+                        double moneyProportion = sum / proportionSum;
+
+                        // Для каждой, кроме последней пропорции из списка найдем соответствующую сумму и добавим ее в список,
+                        // округляя до копеек (сотых).
+                        for (int i = 0; i < propArray.Length - 1; i++)
                         {
-                            result.Append(moneyAmount.ToString("#.##") + ';');
-                            moneyAmount = 0;
+                            finalSums.Add((moneyProportion * propArray[i]).ToString("#.##"));
                         }
-                        // Иначе, добавляем значение из списка в результат и вычитаем это значение из общего кол-ва денег.
-                        else
+
+                        // Определим оставшуюся сумму.  
+                        foreach (string pSum in finalSums)
                         {
-                            result.Append(sumsFromList[i].ToString("#.##") + ";");
-                            moneyAmount -= sumsFromList[i];
+                            sum -= double.Parse(pSum);
                         }
-                    }
-                    // Вернем результат в виде строки с удаленным последним символом ";".
-                    return result.Remove(result.Length - 1, 1).ToString();
+
+                        // Добавим остаток как последнее значение списка.
+                        finalSums.Add(sum.ToString());
+
+                        // Передадим значения из списка в строку на вывод.
+                        foreach (string lSum in finalSums)
+                        {
+                            result.Append(lSum + ";");
+                        }
+
+                        //Вернем строку, удалив в конце ";". 
+                        return result.Remove(result.Length - 1, 1).ToString();
+
+                    case "ПЕРВ":
+                        //Начнем заполнять результирующую строку максимальными значениями с начала списка.
+                        for (int i = 0; i < propArray.Length; i++)
+                        {
+                            // Если кол-во оставшихся денег меньше или равно значению в списке, добавляем остатки и устанавливаем количество денег в ноль.
+                            if (sum <= propArray[i])
+                            {
+                                finalSums.Add(sum.ToString());
+                                sum = 0;
+                            }
+                            // Иначе, добавляем значение из списка в результат и вычитаем это значение из общего кол-ва денег.
+                            else
+                            {
+                                finalSums.Add(propArray[i].ToString());
+                                sum -= propArray[i];
+                            }
+                        }
+
+                        // Передадим значения из списка в строку на вывод.
+                        foreach (string lSum in finalSums)
+                        {
+                            result.Append(lSum + ";");
+                        }
+
+                        //Вернем строку, удалив в конце ";". 
+                        return result.Remove(result.Length - 1, 1).ToString();
+
+                    case "ПОСЛ":
+                        //Начнем заполнять результирующую строку максимальными значениями с конца списка.
+                        for (int i = propArray.Length - 1; i >= 0; i--)
+                        {
+                            // Если кол-во оставшихся денег меньше или равно значению в списке, добавляем остатки и устанавливаем количество денег в ноль.
+                            if (sum <= propArray[i])
+                            {
+                                finalSums.Add(sum.ToString());
+                                sum = 0;
+                            }
+                            // Иначе, добавляем значение из списка в результат и вычитаем это значение из общего кол-ва денег.
+                            else
+                            {
+                                finalSums.Add(propArray[i].ToString());
+                                sum -= propArray[i];
+                            }
+                        }
+
+                        finalSums.Reverse();
+
+                        // Передадим значения из списка в строку на вывод.
+                        foreach (string lSum in finalSums)
+                        {
+                            result.Append(lSum + ";");
+                        }
+
+                        //Вернем строку, удалив в конце ";". 
+                        return result.Remove(result.Length - 1, 1).ToString();
+                }
             }
 
-            return "";
+            return "Invalid option. Please, try again.";
         }
     }
 }
