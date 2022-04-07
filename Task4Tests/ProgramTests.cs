dcfa3ac2ccc5c80038bc9fcf197f9aa894f20d5b
User <User@DESKTOP-11US3S7>
2022-04-07 13:46:50 +0300

    Updated Unit tests.

--- Task4Tests/ProgramTests.cs
+++ Task4Tests/ProgramTests.cs
@@ -13,7 +13,7 @@ namespace Task4.Tests
     {
 
         [TestMethod()]
-        public void DistributeTheSumTestIfSumIsNaN()
+        public void DistributeTheSum_WhenSumIsNan_ShouldThrowArumentException()
         {
             Action action = () => Task4.Program.DistributeTheSum("ПОСЛ", double.NaN, "1000;2000;3000;5000;8000;5000");
             ArgumentException argumentException = Assert.ThrowsException<ArgumentException>(action);
@@ -22,7 +22,7 @@ namespace Task4.Tests
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfSumIsPositiveInfinity()
+        public void DistributeTheSum_WhenSumIsPositiveInfinity_ShouldThrowArgumentException()
         {
             Action action = () => Task4.Program.DistributeTheSum("ПОСЛ", double.PositiveInfinity, "1000;2000;3000;5000;8000;5000");
             ArgumentException argumentException = Assert.ThrowsException<ArgumentException>(action);
@@ -31,7 +31,7 @@ namespace Task4.Tests
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfSumIsLessThanOrEqualToZero()
+        public void DistributeTheSum_WhenSumIsNegativeInfinity_ShouldThrowArgumentException()
         {
             Action action = () => Task4.Program.DistributeTheSum("ПОСЛ", double.NegativeInfinity, "1000;2000;3000;5000;8000;5000");
             ArgumentException argumentException = Assert.ThrowsException<ArgumentException>(action);
@@ -40,7 +40,7 @@ namespace Task4.Tests
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfOptionIsNotValid()
+        public void DistributeTheSum_WhenOptionIsInvalid_ShouldThrowArgumentException()
         {
             Action action = () => Task4.Program.DistributeTheSum("GJC", 10000, "1000;2000;3000;5000;8000;5000");
             ArgumentException argumentException = Assert.ThrowsException<ArgumentException>(action);
@@ -49,28 +49,28 @@ namespace Task4.Tests
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfOptionIsPROP()
+        public void DistributeTheSum_WhenOptionIsPROP_ReturnsProtoptionsOfSumByPROP()
         {
-            string result = Task4.Program.DistributeTheSum("ПРОП", 10000, "1000;2000;3000;5000;8000;5000");
-            string expected = "416,67;833,33;1250;2083,33;3333,33;2083,34";
+            var result = Task4.Program.DistributeTheSum("ПРОП", 10000, "1000;2000;3000;5000;8000;5000");
+            var expected = "416,67;833,33;1250;2083,33;3333,33;2083,34";
 
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfOptionIsPERV()
+        public void DistributeTheSum_WhenOptionIsPERV_ReturnsProportionsOfSumByPERV()
         {
-            string result = Task4.Program.DistributeTheSum("ПЕРВ", 10000, "1000;2000;3000;5000;8000;5000");
-            string expected = "1000;2000;3000;4000;0;0";
+            var result = Task4.Program.DistributeTheSum("ПЕРВ", 10000, "1000;2000;3000;5000;8000;5000");
+            var expected = "1000;2000;3000;4000;0;0";
 
             Assert.AreEqual(expected, result);
         }
 
         [TestMethod()]
-        public void DistributeTheSumTestIfOptionIsPOSL()
+        public void DistributeTheSum_WhenOptionIsPOSL_ReturnsProportionsOfSumByPOSL()
         {
-            string result = Task4.Program.DistributeTheSum("ПОСЛ", 10000, "1000;2000;3000;5000;8000;5000");
-            string expected = "0;0;0;0;5000;5000";
+            var result = Task4.Program.DistributeTheSum("ПОСЛ", 10000, "1000;2000;3000;5000;8000;5000");
+            var expected = "0;0;0;0;5000;5000";
 
             Assert.AreEqual(expected, result);
         }
